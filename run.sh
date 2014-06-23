82b6271721f9f8046af7f98db03b1f8990af4dcb
Christopher Bradshaw <christopher.bradshaw@clever.com>
2014-06-23 15:45:02 -0700

    added bin in package.json

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 dir='node_modules/coffeescript-style-guide'
-coffeelint=$dir'/node_modules/coffeelint/bin/coffeelint'
+coffeelint=$dir'/node_modules/.bin/coffeelint'
 
 find . -type f -path "*.coffee" | grep -v node_modules | xargs $coffeelint -f $dir/coffeelint-config.json
