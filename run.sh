3e32c37b2b1297721c064f85bcdf46242aa049d0
Christopher Bradshaw <christopher.bradshaw@clever.com>
2014-06-27 10:58:16 -0700

    Improved file finding. Now takes args

--- run.sh
+++ run.sh
@@ -1,4 +1,16 @@
+#!/bin/sh
+
 dir='node_modules/coffeescript-style-guide'
 coffeelint=$dir'/node_modules/.bin/coffeelint'
 
-find . -type f -path "*.coffee" | grep -v node_modules | xargs $coffeelint -f $dir/coffeelint-config.json
+if [ $# -ne 0 ]; then
+  for arg in $*; do
+    $coffeelint -f $dir/coffeelint-config.json $arg
+  done
+else
+  if [ -d ".git" ]; then
+    git ls-files | grep -E .coffee$ | xargs $coffeelint -f $dir/coffeelint-config.json
+  else
+    find . -type f -path "*.coffee" | grep -v node_modules | xargs $coffeelint -f $dir/coffeelint-config.json
+  fi
+fi
